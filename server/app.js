9f0c42cf0a3d8dd5422a0b9f3b3180127537c7d9
knowledge <abhi7ty4@gmail.com>
2024-06-30 14:23:51 +0530

    adding original url on the both side

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors(
   {
-    origin: "https://restaurant-app-ine.vercel.app/",
+    origin:[ "https://restaurant-app-ine.vercel.app/"],
     credentials: true
   }
 ));
