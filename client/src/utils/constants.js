12d75cd9d70fb1ec323a7444afacd716a3100623
knowledge <abhi7ty4@gmail.com>
2024-06-30 15:50:05 +0530

    fixing bugs

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,9 +1,9 @@
 
 export const API = {
-  login: "https://restaurant-app-api.vercel.app/" + "auth/login",
-  register: "https://restaurant-app-api.vercel.app/" + "auth/register",
-  getRestaurants: "https://restaurant-app-api.vercel.app/" + "getRestaurants",
-  addOrder: "https://restaurant-app-api.vercel.app/" + "addorder",
-  getOrders: "https://restaurant-app-api.vercel.app/" + "getorders",
-  verifyAuth: "https://restaurant-app-api.vercel.app/" + "verifyauth",
+  login: "https://restaurant-app-ine.onrender.com/" + "auth/login",
+  register: "https://restaurant-app-ine.onrender.com/" + "auth/register",
+  getRestaurants: "https://restaurant-app-ine.onrender.com/" + "getRestaurants",
+  addOrder: "https://restaurant-app-ine.onrender.com/" + "addorder",
+  getOrders: "https://restaurant-app-ine.onrender.com/" + "getorders",
+  verifyAuth: "https://restaurant-app-ine.onrender.com/" + "verifyauth",
 };
